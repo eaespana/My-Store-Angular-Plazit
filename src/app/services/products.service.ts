83c8c5d76c9872e2437e26490d5d7a6f7473725a
Alejandro España <alejo-e-1@hotmail.com>
2022-08-28 22:44:18 -0500

    feat: agragar proxy CORS

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpParams } from '@angular/common/http'
+import { HttpClient,HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators'
 
 import { Product, createProductDTO, UpdateProductDTO } from '../models/product.model';
 
@@ -8,7 +9,7 @@ import { Product, createProductDTO, UpdateProductDTO } from '../models/product.m
 })
 export class ProductsService {
 
-  private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
+  private apiUrl = '/api/products';
 
   constructor(
     private httpClient: HttpClient
@@ -20,7 +21,10 @@ export class ProductsService {
       params = params.set('limit',limit);
       params = params.set('offset',offset);
     }
-    return this.httpClient.get<Product[]>(this.apiUrl, {params});
+    return this.httpClient.get<Product[]>(this.apiUrl, {params})
+    .pipe(
+      retry(3)
+    );
   }
 
   getProduct(id: string) {
